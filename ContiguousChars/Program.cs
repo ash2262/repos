453a177a98ef8a744aabdcf1617d631dfd9585c3
ash2262 <ash2262@gmail.com>
2022-07-09 23:40:19 +0530

    9july2022
    9july2022

---
+++ ContiguousChars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContiguousChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RedundantBraces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace RedundantBraces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "((a+b))";
+            Console.WriteLine(braces(A)); 
+        }
+        public static int braces(string A)
+        {
+            var stack = new Stack<char>();
+            foreach (var ch in A)
+            {
+                if (ch !=')'){
+                    stack.Push(ch);
+                }else
+                {
+                    int token = 0;
+                    while (stack.Peek() !='('){
+                        stack.Pop();
+                        token++;
+                    }
+                    if (token == 0 || token == 1)
+                    {
+                        return 1;
+                    }
+                    stack.Pop();
+                }
+            }
+            return 0;
+        }
+    }
+}
